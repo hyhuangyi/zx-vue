cfdfd52f1be98e8f2334ad06d02b85c0f665a63e
hyhuangyi <zx597505910@outlook.com>
2019-11-21 21:36:54 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     outputDir: 'dist',
     devServer: {
         open: true,//启动项目后自动开启浏览器
-        host: 'localhost',//ip
+        host: '0.0.0.0',//ip
         port: 8087,//端口号
         https: false,//是否开启协议名,如果开启会发出警告
         hotOnly: false,
