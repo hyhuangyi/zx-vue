3696eccd4e7a717495fdcac0fa033877c02a5640
huangy <597505910@qq.com>
2020-05-23 12:08:50 +0800

    -mvideo 封面和url

--- src/components/page/sys/video.vue
+++ src/components/page/sys/video.vue
@@ -27,11 +27,10 @@ export default {
                 sources: [
                     {
                         type: '',
-                        src: 'https://vdept.bdstatic.com/356e74584533766b464a7a41757a6a44/6e7a393847343265/f709ba8a82d3abf5a2f63f53301b8c46cfc1a52e72bea843fc9bbe082cb9c763a5e2e5bddd6187ee630a7dd5e12edbf9dc7ed7a1c5246c7d2d396f7d78068afd.mp4?auth_key=1590172193-0-0-b4e6c8c809af4159e7b9e348e3cbe0c5' //url地址
-                        // src:'https://apd-bd3bce43a6617c6e3bef8ec349de7635.v.smtcdns.com/om.tc.qq.com/Ak6M-7uyKUsbX3EDyGXdqVUmu0Vk3zCcPfL2SD-YQzX8/uwMROfz2r5zCIaQXGdGnC2dfDmavB6NbsWKJAiRTQBPbT70a/b0351k14z90.mp4?sdtfrom=v1010&amp;guid=f5c61c8cc67e330d64cb25494ae2258b&amp;vkey=12A1BB6A64A957C246883854FC957BE94B549CA93A792D336F1B3F5B180EBAF0C6953BDEB992F794EC95ED031350F183996CEC3B40D00B48E548B8B6CD35799E294FB360922AA90A367FAE5914C72CC8B62746B9AAEE164312311CBBD49F849CBDDBD4D32B5F0B0A6267F91435025DDAEC197F331C678E1A41619713D1262A74';
+                        src: 'http://cloud.video.taobao.com/play/u/3926817755/p/1/e/6/t/1/252977337599.mp4' //url地址
                     }
                 ],
-                poster: 'http://5b0988e595225.cdn.sohucs.com/images/20180211/628567caf18c40feb492467a80e3c795.jpeg', //你的封面地址
+                poster: 'https://img.alicdn.com/tfs/TB1nCfgf1bviK0jSZFNXXaApXXa-1672-938.png', //你的封面地址
                 // width: document.documentElement.clientWidth,
                 notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
                 controlBar: {
