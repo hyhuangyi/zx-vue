3142e21c4fae7395473238b16d51d4ef90b00e67
huangy <597505910@qq.com>
2021-12-06 15:48:25 +0800

    update 12-6

--- src/utils/globalApi.js
+++ src/utils/globalApi.js
@@ -1,5 +1,6 @@
 // const baseURL = "http://localhost:8088/zx"
-const baseURL="http://47.110.13.117:8088/zx"
+// const baseURL="http://47.110.13.117:8088/zx"
+const baseURL="http://150.158.86.218/:8088/zx"
 
 export default {
   baseURL
