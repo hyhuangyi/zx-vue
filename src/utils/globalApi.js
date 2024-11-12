ab9f1bf92084736ec4316ea275f50c92a215eacc
huangy <huangy@huonu.com>
2024-11-12 15:32:40 +0800

    22

--- src/utils/globalApi.js
+++ src/utils/globalApi.js
@@ -3,7 +3,7 @@
 // const baseURL="http://47.110.13.117:8088/zx"
 // const baseURL="http://150.158.86.218:8088/zx"
 
-const baseURL="http://120.27.201.175/:8088/zx"
+const baseURL="http://120.27.201.175:8088/zx"
 
 export default {
   baseURL
